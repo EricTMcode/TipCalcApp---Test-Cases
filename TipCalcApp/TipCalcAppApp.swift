39bcbde102c8b09d69b5acfda97ba94dd3a20f74
Eric <ebagnaro@gmail.com>
2024-03-21 06:24:32 +0100

    Initial Commit

---
+++ TipCalcApp/TipCalcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcAppApp.swift
+//  TipCalcApp
+//
+//  Created by Eric on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
